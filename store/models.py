32a505920d365b99cb36a1455c43f53ecafbeb30
Mukund Khandelwal <khandelwalmukund23@gmail.com>
2021-12-31 19:24:28 +0530

    models

--- store/models.py
+++ store/models.py
@@ -46,10 +46,10 @@ class OrderItem(models.Model):
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
-    address = models.CharField(max_length=200, null=True)
-    city = models.CharField(max_length=200, null=True)
-    state = models.CharField(max_length=200, null=True)
-    zipcode = models.CharField(max_length=200, null=True)
+    address = models.CharField(max_length=150, null=True)
+    city = models.CharField(max_length=100, null=True)
+    state = models.CharField(max_length=100, null=True)
+    zipcode = models.CharField(max_length=150, null=True)
     data_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
